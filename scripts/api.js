160aa243bced0b79da4e318cebdac6799e69de6b
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-07 10:46:30 -0700

    moving it along

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,34 @@
+/*eslint-env jquery*/
 // eslint-disable-next-line no-unused-vars
 'use strict';
 
-const api = function() {
+const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joaquin';
   const getItems = function(callback) {
-    callback('api module works');
+    // $.getJSON(BASE_URL + '/items', callback);
+    // callback('api module works');
+  };
+
+  const createItem = function(name, callback) {
+    const newItem = {
+      name
+    };
+
+    const stringNewItem = JSON.stringify(newItem);
+
+    $.ajax({
+      url: { BASE_URL } / items,
+      method: 'POST',
+      contentType:'application/json',
+      data: 'stringNewItem',
+      success: (response) =>{
+        callback(response);
+        console.log('this is the .ajax call');
+      }
+    });
   };
   return {
-    getItems
-  }
-
-}();
+    getItems,
+    createItem
+  };
+})();
